a359a68fd66b369d36fcba460e0b758063a9adc6
NguyenDinhLuu211 <131143370+NguyenDinhLuu211@users.noreply.github.com>
2023-10-23 14:06:56 +0700

    Lab4

---
+++ Lab4/Bai 3/script.js
@@ -0,0 +1,10 @@
+window.onload = function() {
+    var a = [
+        'Chào buổi sáng',
+        'Chào buổi tối',
+        'Chào bạn',
+        'Chào ',
+        'Hi'
+    ];
+    alert(a[Math.floor(Math.random()* a.length)]); 
+};
